d59003c552c79d21a6eb2762493364bd1dc407f8
Joachim Laviolette <joachim.laviolette@gmail.com>
2017-05-29 13:40:22 +0200

    Add files via upload

---
+++ utiles/footer.php
@@ -0,0 +1,38 @@
+<footer>
+	<div id="copyright">
+		<p class="copyright" charset="utf-8">Tous droits réservés à Dolby-Series.fr | 2017</p>
+	</div>
+	<div id="links">
+		<ul>
+			<li><a onclick="open_popup_dev()">Développeurs</a></li>
+			<li>|</li>
+			<li><a onclick="open_popup_contact()"">Contact</a></li>
+		</ul>
+	</div>
+	<div id="popup_dev_bg"></div>
+	<div id="popup_dev">
+		<div id="close_popup"><img onclick="close_popup_dev()" src="/medias/croix_popup.jpg"/></div>
+		<p class="popup_footer_title">Développeurs</p>
+		<div class="popup_footer_content">
+			<p>Ce site a été réalisé par deux jeunes développeurs : Joachim LAVIOLETTE et Vincent MAQUIABA. Nous avons développé ce site dans le cadre d'un projet de programmation Web. Ce projet consistait à reproduire un site au style de iMDB ou encore d'Allociné, dans le but de rassemblerles informations de séries télévisées, dans un premier temps, ainsi que permettre à la communauté des "sérievores" d'échanger et de partager leur avis par la mise en place d'un <a href="forum.php">forum</a>.
+			<p>Nous avons mené ce projet lors du second semestre de 3e année Licence informatique. Les langages que nous avons utilisés sont le : Php et le JavaScript. Nous avons notamment utilisé des technologies et des librairies comme l'AJAX et le jQuery pour réaliser certaines fonctionnalités d'insertions et de contrôle de données.
+			</p>
+			<p>Pour toute information supplémentaire, dirigez-vous sur notre page <a>"Contact"</a>. Nous vous souhaitons à tous une bonne visite !</p>
+		</div>
+	</div>
+	<div id="popup_contact_bg"></div>
+	<div id="popup_contact">
+		<div id="close_popup"><img onclick="close_popup_contact()" src="/medias/croix_popup.jpg"/></div>
+		<p class="popup_footer_title">Contact</p>
+		<div class="popup_footer_content">
+			<p class="dev_mails">Joachim LAVIOLETTE - joachim.laviolette[at]gmail.com - Linkedin : <a href="https://fr.linkedin.com/in/joachim-laviolette-2a3603114">https://fr.linkedin.com/in/joachim-laviolette-2a3603114</a>
+			<br/>Vincent MAQUIABA - vincent.maquiaba[at]gmail.com - Linkedin : <a href="https://fr.linkedin.com/in/vincent-maquiaba-aa2381bb">https://fr.linkedin.com/in/vincent-maquiaba-aa2381bb</a></p>
+			<div id="contact_adresse">
+				<p>Université de Villetaneuse | Paris 13 - Institut Galilée</p>
+				<p><u>Adresse:</u> 99 Avenue Jean Baptiste Clément, 93430 Villetaneuse</p>
+				<p><u>Chargé de matière:</u> Julien DAVID - LIPN : <a href="https://lipn.univ-paris13.fr/~david/">Site internet</a></p>
+				<img class="contact_logo" src="/medias/logo_paris13.jpg"/>
+			</div>
+		</div>
+	</div>
+</footer>
